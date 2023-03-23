6c5f4c826bd1e0d296db5ecad47862d510d1900e
cesaramelo <cavmelo@icomp.ufam.edu.br>
2023-03-23 08:54:12 -0400

    Documentos & intercalação

---
+++ Modulo01/docs/readme.md
@@ -0,0 +1,8 @@
+# Sobre estes documentos
+
+Nesta pasta você irá encontrar os documentos produzidos para o módulo 01 da disciplina.
+
+* Dicas
+* Quiz
+* Enunciados
+
--- Modulo01/src/intercalando3.c
+++ Modulo01/src/intercalando3.c
@@ -22,15 +22,27 @@ void intercalar(double notas01[], int tam_notas01, double notas02[], int tam_not
     }
 
 }
+
+void ler_notas(double vetor_notas[], int tam){
+    for(int i=0;i<tam;i++){
+        scanf("%lf", &vetor_notas[i]);
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     int tam_EC = 3;
     int tam_CC = 3;
     int tam_ES = 3;
 
-    double notas_EC[] = {8.0,8.0,8.0};
-    double notas_CC[] = {8.0,8.5,9.0};
-    double notas_ES[] = {7.5, 9.0, 10.0};
+    double notas_EC[tam_EC];
+    double notas_CC[tam_CC];
+    double notas_ES[tam_ES];
+
+    ler_notas(notas_EC, tam_EC);
+    ler_notas(notas_CC, tam_CC);
+    ler_notas(notas_ES, tam_ES);
+
     double medias_CC_EC[tam_CC+tam_EC];
     double medias[tam_CC+tam_EC+tam_ES];
     
