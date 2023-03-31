4770d8fd82bdc8da873291afe022025b4588fe79
Cesar Melo <cavmelo@gmail.com>
2023-03-31 18:15:42 -0400

    Inserir no inicio da lista e conceitos da lista

---
+++ Modulo01/docs/slides.md
@@ -0,0 +1,41 @@
+---
+marp: true
+title: Sobre listas
+---
+# Sobre Lista
+
+---
+# Mas antes, Conceitos abstratos
+
+| Conceito      | Descrição     | Atributos      |
+| -----------   | -----------   | ---            |
+| Carro         | automóvel, meio de transporte terrestre              |  nro rodas, cor, chasi, nro portas              |
+| Celular       |  aparelho de comunicação| cor, modelo, marca,  sistema operacao, cameras, memória,  operadora, tamanho da tela |
+| Lista         | agrupador de elementos |  Tamanho, Tipo de Informação, Tem ordenação, 
+
+
+
+---
+
+# Definição
+__Lista lineares são estruturas de dados permitem o armazenamento de dados em memória de forma não contínua.__
+
+----
+# Propriedades
+Uma lista $L$ com $n$ elementos tem as seguintes propriedades:
+
+* Seja $l_i$ um elemento de L, então $l_{i-1}$ prescede $l_i$ e $l_{i+1}$ sucede $l_i$;
+    * Último elemento de $L$, $l_n$ não tem sucessor;
+    * Primeiro elemento de $l$, $l_1$ não tem predecessor.
+* Para acessar o elemento $l_i$ é preciso acessar todos os elementos $l_j$, $j=1,...,i$.
+
+
+----
+# Representação
+
+
+______
+
+
+
+
--- Modulo01/src/listase.c
+++ Modulo01/src/listase.c
@@ -1,12 +1,16 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+/* Tipo elemento da LSE*/
 typedef struct elem_lse t_elemento_lse;
 struct elem_lse{
     int cargautil;// carga util
     struct elem_lse* prox;// endereco prox
 };
 
+/**
+ * cria um elemento da LSE
+*/
 t_elemento_lse* criar_elemento_lse(int cargautil){
     t_elemento_lse* novo = malloc(sizeof(t_elemento_lse));
     novo->cargautil = cargautil;
@@ -15,24 +19,35 @@ t_elemento_lse* criar_elemento_lse(int cargautil){
     return novo;
 }
 
-typedef struct LSE{
-    t_elemento_lse* inicio;
-    // int tamanho;
+/**
+ * Definição do tipo Lista Simplesmente Encadeada
+*/
+typedef struct lse{
+    t_elemento_lse* inicio; // Primeiro elemento
+    // int tamanho; 
     // int nro_ins;
     // int nro_del;
-}T_LSE;
+}t_lse;
 
-T_LSE* criarLSE(){
-
-    T_LSE* nova = malloc(sizeof(struct LSE));
+t_lse* criar_lse(){
+     t_lse* nova;
+     
+    nova  = malloc(sizeof(t_lse));
     nova->inicio = NULL;
 
     return nova;
 }
 
 // operacoes baseadas em posicao
-// inserir_inicioLSE();
-// remover_inicioLSE();
-// acessar_LSE();
-// inserir_fincalLSE();
-// remover_finalLSE();
+void inserir_inicio_lse(t_lse* lse, int carga){
+    
+    t_elemento_lse* novo = criar_elemento_lse(carga);
+
+    novo->prox = lse->inicio;
+    lse->inicio = novo;
+
+}
+// int remover_inicio_lse(){}
+// acessar_lse(){}
+// void inserir_final_lse(){}
+// int remover_final_lse(){}
