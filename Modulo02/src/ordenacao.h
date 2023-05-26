48b986d3bab3628a9e71ee4a55a4899d19d95adf
cavmelo <cavmelo@gmail.com>
2023-05-26 15:07:32 -0400

    correcao de bugs e usa*

--- Modulo02/src/ordenacao.h
+++ Modulo02/src/ordenacao.h
@@ -3,7 +3,7 @@ typedef int(*TCompararQS)(void*, void*);
 typedef int(*TLerIdRadixSort)(void*);
 typedef int(*TLerIdContagem)(void*);
 
-void heapsort(void* vetor[], int tam, TCompararHeapsort comparar);
+void heap_sort(void* vetor[], int tam, TCompararHeapsort comparar);
 void quicksort(void* vetor[], int lim_i, int lim_f, TCompararQS comparar);
-void radixsort(void* vetor[], int tam, int digs, TLerIdRadixSort ler_id);
-void contagem(void* vetor[], int tam, TLerIdContagem ler_id);
+void radix_sort(void* vetor[], int tam, int digs, TLerIdRadixSort ler_id);
+void contagem(void* vetor[], int tam, int tam_c, TLerIdContagem ler_id);
