28c074f70edba39a66b5fefc7c4418affa3475d8
cavmelo <cavmelo@gmail.com>
2023-06-02 15:26:45 -0400

    AB inserção neutralizada

--- Modulo03/src/ab.c
+++ Modulo03/src/ab.c
@@ -1,4 +1,7 @@
 #include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+#include "string.h"
 
 typedef struct no TNo;
 struct no{
@@ -18,65 +21,107 @@ TNo* criar_no(TNo* ascentral, void* info){
     return novo;
 }
 
-conectaRaizSAE(TNo *raiz, TNo* sae){
+typedef void(*TImprimirAB)(void* info);
+typedef struct ab{
+    TNo* raiz;
+    TImprimirAB imprimir;
+    // removerInfoNo
+} t_ab;
+
+t_ab* criarAB(TImprimirAB imprimir){
+    t_ab* ab = malloc(sizeof(t_ab));
+
+    ab->raiz = NULL;
+    ab->imprimir = imprimir;
+
+    return ab;
+}
+
+
+void conectaRaizSAE(TNo *raiz, TNo* sae){
     raiz->sae = sae;
     sae->ancestral = raiz;
 }
 
-conectaRaizSAD(TNo *raiz, TNo* sad){
+void conectaRaizSAD(TNo *raiz, TNo* sad){
     raiz->sad = sad;
     sad->ancestral = raiz;
 }
 
-TNo* inserirAB(TNo* raiz, void* info){
+static TNo* _inserirAB(TNo* raiz, void* info){
+    //printf("Iniciou inserção \n");
     if (raiz == NULL){
+        //printf("Inseriu no \n");
         TNo* novo = criar_no(raiz,info);
         return novo;
     }
     if ((rand()%2) == 1){ // relação aleatória
-        TNo* sae = inserirAB(raiz->sae, info);
+        TNo* sae = _inserirAB(raiz->sae, info);
         conectaRaizSAE(raiz, sae);
     }else{
-        TNo* sad = inserirAB(raiz->sad, info);
-        concataRaizSAD(raiz->sad, sad);
+        TNo* sad = _inserirAB(raiz->sad, info);
+        conectaRaizSAD(raiz, sad);
     }
+
+    return raiz;
+}
+
+void inserirAB(t_ab *ab, void* info){
+
+    ab->raiz = _inserirAB(ab->raiz, info);
+    //printf("Terminou insercao\n");
+
 }
 
 #define MAX(a,b) (a>b?a:b)
 
-int alturaAB(TNo *raiz){
+static int _alturaAB(TNo *raiz){
     if (raiz == NULL)
         return -1;
-    return MAX(alturaAB(raiz->sae), alturaAB(raiz->sad))+1;
+    return MAX(_alturaAB(raiz->sae), _alturaAB(raiz->sad))+1;
 }
 
-int tamanhoAB(TNo* raiz){
+int alturaAB(t_ab *ab){
+    return _alturaAB(ab->raiz);
+}
+
+static int _tamanhoAB(TNo* raiz){
     if (raiz == NULL)
         return 0;
-    return (tamanhoAB(raiz->sae)+tamanhoAB(raiz->sad))+1;
+    return (_tamanhoAB(raiz->sae)+_tamanhoAB(raiz->sad))+1;
 }
 
-imprimirAB(TNo* raiz){
+int tamanhoAB(t_ab *ab){
+    return _tamanhoAB(ab->raiz);
+
+}
+
+static void _imprimirAB(TNo* raiz, TImprimirAB imprimir){
     if (raiz == NULL)
         return;
-    imprimirAB(raiz->sae);
-    printf("%p",raiz->info);
-    imprimirAB(raiz->sad);
+    _imprimirAB(raiz->sae, imprimir);
+    imprimir(raiz->info);
+    _imprimirAB(raiz->sad, imprimir);
 }
 
+void imprimirAB(t_ab* ab){
+    _imprimirAB(ab->raiz, ab->imprimir);
+}
 //====== USA AB
 
 typedef struct carro{
     char placa[8];
+    int anoFabricacao;
     int hora, min;
 }t_carro;
 
-t_carro* criar_carro(char placa[], int hora, int min){
+t_carro* criar_carro(char placa[], int hora, int min, int anoFab){
     t_carro *c = malloc(sizeof(t_carro));
 
     strcpy(c->placa,placa);
     c->hora = hora;
     c->min = min;
+    c->anoFabricacao = anoFab;
 
     return c;
 }
@@ -87,18 +132,35 @@ void destroy_carro(t_carro *c){
 
 void imprimir_carro(t_carro* c){
 
-    printf("Placa: %s %d:%d\n", c->placa, c->hora, c->min);
+    printf("Placa: %s %d:%d %d\n", c->placa, c->hora, c->min, c->anoFabricacao);
 
 }
 
 int main(int argc, char const *argv[])
 {
+    char placa[9];
+    int hora, min, anoFab;
+
     srand(time(NULL));
 
-    // leitura para inserir os dados AB;
+    t_ab *carros = criarAB(imprimir_carro);
 
-    //imprimirAB(raiz);
-    //alturaAB(raiz);
-    //tamanhoAB(raiz->sad);
+    // leitura para inserir os dados AB;
+    scanf("%s", placa);    
+    while(strcmp(placa,"FIM")!=0){
+        scanf("%d", &hora);
+        scanf("%d", &min);
+        scanf("%d", &anoFab);
+        t_carro* novo = criar_carro(placa, hora, min, anoFab);
+       
+        inserirAB(carros, novo);
+       
+        scanf("%s", placa);    
+    }
+
+    imprimirAB(carros);
+    printf("Altura: %d\n", alturaAB(carros));
+    printf("Tamanho: %d\n", tamanhoAB(carros)); 
+
     return 0;
 }
